9f8fdcd56036481c02d9e15824f7f30ea04a8cab
ssramdin <61013882+ssramdin@users.noreply.github.com>
2020-12-08 12:15:06 -0300

    Problem 2 cost calculation completed (verification pending)

--- Assign_1/Problem_2/Calc_cost.py
+++ Assign_1/Problem_2/Calc_cost.py
@@ -19,12 +19,15 @@ File3       = '3_Hotel_Costs_Group_34.xlsx'
 
 Timetable   =  pd.read_excel(File1, usecols = 'A:E')
 Duty_period =  pd.read_excel(File2, usecols = 'A:B')
-Hotel       =  pd.read_excel(File3, usecols = 'A:c')
+Hotel       =  pd.read_excel(File3, usecols = 'A:C')
+
+Airports    =  Hotel.iloc[:, 0].to_numpy()
+Room_fee    =  Hotel.iloc[:, 2].to_numpy()
 
 Flights_duty     =  Duty_period.iloc[:, 1].to_numpy()
 
 Flight_num  = Timetable.iloc[:, 0].to_numpy()
-
+Origin      = Timetable.iloc[:, 1].to_numpy()
 T_start     = Timetable.iloc[:, 3].to_numpy()
 T_end       = Timetable.iloc[:, 4].to_numpy()
 
@@ -41,9 +44,6 @@ for i in range(len(T_start)):
 Flights_duty = np.array([Flights_duty[i][1:-1] for i in range(len(Flights_duty))])
 Flights_duty = np.array([i.replace("'", '') for i in Flights_duty ])
 
-
-
-
 Cost = np.ones(len(Flights_duty))
 
 for i in range(len(Flights_duty)):
@@ -56,9 +56,9 @@ for i in range(len(Flights_duty)):
          cost_hour   =  (Cap_h + firstO_h +Steward_h)*h
          Flight_cost = Flight_cost + cost_fixed + cost_hour
     if l[0] != l[-1]:
-        hotel_cost = 1  # add costs from file for
+        Base = Origin[list(Flight_num).index(l[0])]
+        hotel_cost =  Room_fee[list(Airports).index(Base)] * 5  # 5 crew members
     else:
         hotel_cost = 0
-
     Cost[i] = Flight_cost + hotel_cost
 

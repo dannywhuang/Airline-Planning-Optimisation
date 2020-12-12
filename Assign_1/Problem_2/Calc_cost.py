482f936cb48195b7f755d05d24331490d1381f1b
Danny <huangdanny9@gmail.com>
2020-12-12 22:07:41 +0100

    added column generation

--- Assign_1/Problem_2/Calc_cost.py
+++ Assign_1/Problem_2/Calc_cost.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 import ast
+from gurobipy import *
+from objectLoader import save_obj
+
+
+
 # ________________________________
 # fixed cost per day
 Cap      = 98
@@ -19,16 +24,17 @@ File2       = '2_Duty_Periods_Group_34.xlsx'
 File3       = '3_Hotel_Costs_Group_34.xlsx'
 
 Timetable   =  pd.read_excel(File1, usecols = 'A:E')
-Duty_period =  pd.read_excel(File2, usecols = 'A:B')
+Duty_period =  pd.read_excel(File2, usecols = 'A:B', converters={'column_name': eval})
 Hotel       =  pd.read_excel(File3, usecols = 'A:C')
 
 Airports    =  Hotel.iloc[:, 0].to_numpy()
 Room_fee    =  Hotel.iloc[:, 2].to_numpy()
 
-Flights_duty     =  Duty_period.iloc[:, 1].to_numpy()
+Flights_duty    =  Duty_period.iloc[:, 1].apply(ast.literal_eval)
 
 Flight_num  = Timetable.iloc[:, 0].to_numpy()
 Origin      = Timetable.iloc[:, 1].to_numpy()
+Destin      = Timetable.iloc[:, 2].to_numpy()
 T_start     = Timetable.iloc[:, 3].to_numpy()
 T_end       = Timetable.iloc[:, 4].to_numpy()
 
@@ -46,17 +52,34 @@ Cost = np.ones(len(Flights_duty))
 
 for i in range(len(Flights_duty)):
     Flight_cost = 0
-    l  = ast.literal_eval(Flights_duty[i])
+    l  = Flights_duty[i]
     for j in l:
          index = list(Flight_num).index(j)
          h     = Flight_time[index]
          cost_hour   =  (Cap_h + firstO_h +Steward_h)*(h+40/60) # add brief periods
          Flight_cost = Flight_cost  + cost_hour
-    if l[0] != l[-1]:
-        Base = Origin[list(Flight_num).index(l[0])]
-        hotel_cost =  Room_fee[list(Airports).index(Base)] * 5  # 5 crew members
+
+    start_duty  = Origin[list(Flight_num).index(l[0])]
+    end_duty    = Destin[list(Flight_num).index(l[-1])]
+
+    if start_duty != end_duty:
+        hotel_cost =  Room_fee[list(Airports).index(end_duty)] * 5  # 5 crew members
     else:
         hotel_cost = 0
     cost_fixed = Cap + firstO + Steward
     Cost[i] = Flight_cost + hotel_cost + cost_fixed
+
+save_obj(Cost, 'cost')
+
+dpf = {}
+for p in range(len(Flights_duty)):
+    for f in range(len(Flight_num)):
+        if Flight_num[f] in Flights_duty[p]:
+            dpf[p, f] = 1
+        else:
+            dpf[p, f] = 0
+
+save_obj(dpf, 'dpf')
+# print("Should be 1: ", dfp[33441, 133]) # verification, should give 1
+# print("Should be 0: ", dfp[33440, 133]) # verification, should give 0
 

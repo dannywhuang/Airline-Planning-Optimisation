c0734eccf8862a01c22a9067066f2956fe7b97df
FVossen <61149685+FVossen@users.noreply.github.com>
2020-12-16 23:17:16 +0100

    Correct PKI

--- Assign_1/Problem_1/KPI.py
+++ Assign_1/Problem_1/KPI.py
@@ -28,7 +28,7 @@ operatingProfit = np.zeros((No_airports,No_airports)) # operating profit
 for i in range(No_airports):
     for j in range(No_airports):
         if i!=j:
-            if z_ij.iloc[i, j + 1] != 0:
+            if z_ij.iloc[i, j] != 0:
                 origin = airports_lst[i]    # To check the current airport origin
                 dest   = airports_lst[j]    # To check the current airport destination
                 distance = funct.calculateDistance(origin, dest)
@@ -39,20 +39,39 @@ for i in range(No_airports):
                 if dest == 'Paris':
                     gj = 0
 
-                direct_ij   = x_ij.iloc[i,j+1] 
+                direct_ij   = x_ij.iloc[i,j] 
                 transfer_ij = 0
                 revenue_w_ij= 0
                 for airport in range(No_airports):  # add transfer passengers that have to first travel to paris to transfer there to their final destination
-                    if i != airport:
-                        origin_w = airports_lst[i]    # To check the current airport origin
-                        dest_w   = airports_lst[airport]    # To check the current airport destination
-                        distance_w_ij = funct.calculateDistance(i,airport)
-                        
-                        wij          = w_ij.iloc[i,airport+1]*(1-gj) + w_ij.iloc[airport,j+1]*(1-gi)
+                    airport_name = airports_lst[airport]
+                    wij          = w_ij.iloc[i,airport]*(1-gj) + w_ij.iloc[airport,j]*(1-gi)    # check if there are any transfer passengers
+
+                    if wij != 0:
+                        if gj == 0:
+                            origin_w = airports_lst[i]          # To check the current airport origin
+                            dest_w   = airports_lst[airport]    # To check the current airport destination
+                        elif gi == 0:
+                            origin_w = airports_lst[airport]    # To check the current airport origin
+                            dest_w   = airports_lst[j]          # To check the current airport destination
+
+                        distance_w         = funct.calculateDistance(origin_w,dest_w)
+                        distance_w_flight1 = funct.calculateDistance(origin_w,'Paris')
+                        distance_w_flight2 = funct.calculateDistance('Paris',dest_w)
+
+                        RPK_w_flight1 = wij * distance_w_flight1
+                        RPK_w_flight2 = wij * distance_w_flight2
+                        RPK_w_ij      = wij * distance_w
+
+                        yield_w_ij    = 5.9 * distance_w ** (-0.76) + 0.043
+                        rev_w_ij      = yield_w_ij * RPK_w_ij
+
+                        distFrac = distance_w_flight1/distance_w_flight2
+                        yield_w_flight1 = distFrac * RPK_w_ij/RPK_w_flight2 * yield_w_ij / (1 + distFrac * RPK_w_flight1/RPK_w_flight2)
+                        yield_w_flight2 = yield_w_flight1/distFrac
+
                         transfer_ij += wij
-                        yield_w_ij   = 5.9 * distance_w_ij**(-0.76) + 0.043   
-                        RPK_w_ij     = wij * distance
-                        revenue_w_ij += yield_w_ij * RPK_w_ij
+
+                        revenue_w_ij += yield_w_flight1 * RPK_w_flight1*(1-gj) + yield_w_flight2 * RPK_w_flight2*(1-gi)
 
                 yield_x_ij   = 5.9 * distance**(-0.76) + 0.043  # yield per RPK
                 RPK_x_ij     = direct_ij * distance
@@ -62,28 +81,31 @@ for i in range(No_airports):
                 RPK_ij    = total_pax * distance
             
                 # Single flight leg related costs
-                zij  = ast.literal_eval(z_ij.iloc[i, j+1])
+                zij  = ast.literal_eval(z_ij.iloc[i, j])
                 ASK_ij = 0
                 Cx_ij  = 0  # fixed operating cost
                 Ct_ij  = 0  # time-dependent cost
                 Cf_ij  = 0  # fuel cost
                 for k in range(len(zij)):
+                    
                     ac_type = zij[k][0]
                     no_flights_ac = zij[k][1]
+                    singleAircraftData = aircraftData.iloc[:, ac_type]
+
                     seats    = float(aircraftData.loc['Seats','Aircraft '+str(int(ac_type+1))])
-                    ac_speed = float(aircraftData.loc['Speed','Aircraft '+str(int(ac_type+1))])
+                    spk = singleAircraftData['Speed']  # speed of aircraft
                     ASK_ij  += seats * distance * no_flights_ac
 
                     # Itinirary cost computations
-                    cx_ij  = float(aircraftData.loc['Fixed operating cost','Aircraft '+str(int(ac_type+1))])
-                    Cx_ij += cx_ij * no_flights_ac
+                    CXk = singleAircraftData['Fixed operating cost']
+                    Cx_ij += CXk * no_flights_ac
 
-                    ct_ij  = float(aircraftData.loc['Time cost parameter','Aircraft '+str(int(ac_type+1))])
-                    Ct_ij += ct_ij * distance/ac_speed * no_flights_ac
+                    cTk = singleAircraftData['Time cost parameter']  # time based costs
+                    Ct_ij += cTk * distance/spk * no_flights_ac
 
-                    cf_ij  = float(aircraftData.loc['Fuel cost parameter','Aircraft '+str(int(ac_type+1))])
-                    Cf_ij += cf_ij * 1.42 / 1.5 * distance * no_flights_ac
-                
+                    cfk = singleAircraftData['Fuel cost parameter']  # fuel cost
+                    Cf_ij += cfk * 1.42 / 1.5 * distance * no_flights_ac
+
                 # weekly load factor on flight leg
                 LF_ij = RPK_ij/ASK_ij
 

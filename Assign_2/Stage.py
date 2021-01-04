3bcb1a4908eb003612cbd00256d3962468f95564
ssramdin <61013882+ssramdin@users.noreply.github.com>
2021-01-04 06:18:18 -0300

    dynamic_progr

--- Assign_2/Stage.py
+++ Assign_2/Stage.py
@@ -9,3 +9,6 @@ class Stage:
         self.binNumber = 29 if time == 120 else floor(time/4)
     def addNode(self, airport):
         self.nodesList[airport.IATA] = Node(airport, self.stageNumber)
+
+
+

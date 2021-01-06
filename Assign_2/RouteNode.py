cc05f120c908752e0f499ef4b6652f997f6fe996
FVossen <61149685+FVossen@users.noreply.github.com>
2021-01-07 00:04:57 +0100

    Added demand update

--- Assign_2/RouteNode.py
+++ Assign_2/RouteNode.py
@@ -1,7 +1,9 @@
 class RouteNode:
-    def __init__(self, IATA, time, profitLeft,cargo):
+    def __init__(self, IATA, time, profitLeft,cargo, stageNumber, binNumber):
         self.IATA = IATA
         self.time = time
         self.profitLeft = profitLeft
         self.cargo      = cargo
+        self.stageNumber = stageNumber
+        self.binNumber = binNumber
 
